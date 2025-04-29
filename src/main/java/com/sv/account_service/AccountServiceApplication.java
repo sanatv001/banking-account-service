6800aeed0236bfdbdf97de82cde18ff66be7b234
sanatv001 <sanatv208@gmail.com>
2025-04-29 20:34:00 +0530

    Initial commit account service

---
+++ src/main/java/com/sv/account_service/AccountServiceApplication.java
@@ -0,0 +1,15 @@
+package com.sv.account_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+//@EnableEurekaClient
+public class AccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountServiceApplication.class, args);
+	}
+
+}
